4e39f1f431c64ffe60fd3b58e8833567ed2fa32f
Carl Yu <yuaooauy@yeah.net>
2018-09-29 01:00:30 -0700

    comment

--- chatbot.py
+++ chatbot.py
@@ -14,10 +14,12 @@ import threading
 from ACAutomation import ACAutomation
 from get_sentiment import get_sentiment
 
+# get sign of a number
 def sign(num):
     sig = num and (1, -1)[num < 0]
     return int(sig)
 
+# exit method
 def CheckBye(str):
     if str.lower() == "bye"\
             or str.lower() == "quit"\
@@ -27,15 +29,18 @@ def CheckBye(str):
     else:
         return False
 
+# print char by char
 def mechPrint(str):
     for char in str:
         time.sleep(0.05)
         print(char, end='')
 
+# text to speech
 def speak(str):
     t2sEngine.say(str)
     t2sEngine.runAndWait()
 
+# threading for mech-like output
 def mechOutput(str):
     task1 = threading.Thread(mechPrint(str + "\n"))
     task2 = threading.Thread(speak(str))
@@ -44,6 +49,7 @@ def mechOutput(str):
     task1.join()
     task2.join()
 
+# open file
 with open("positive-words.txt") as f:
     lines = f.readlines()
 positive_words = [line.strip() for line in lines]
@@ -62,6 +68,7 @@ for negative_word in negative_words:
     negative_acAutomation.insert(negative_word)
 negative_acAutomation.build()
 
+# q&a pool
 question = ["So how's your day, ",
             "How are you today, ",
             "How are you doing, ",
@@ -70,10 +77,12 @@ question = ["So how's your day, ",
             ]
 answer = [["That sucks", "Bummer"], ["Hmm..", "I see"], ["Great", "Cool"]]
 
+# text 2 speech init
 t2sEngine = pyttsx.init()
 
-
+# main loop
 while 1:
+    # choose between basic version and google sentiment analysis version
     while 1:
         mode = raw_input("Please choose chat bot version: 1-basic, 2-sentiment. Type 1 or 2.\n")
         if mode == "1" or mode == "2":
@@ -85,14 +94,14 @@ while 1:
     if CheckBye(name):
         break
 
+    # randomize the question
     questionIndex = random.randint(1, 100) % 4
     mechOutput(question[questionIndex] + name + "?")
     howIsTheDay = raw_input()
     if CheckBye(howIsTheDay):
         break
 
-    # basic info
-
+    # basic info statistics
     lettersCnt = len(howIsTheDay)
     howIsTheDay = re.sub('[^0-9a-zA-Z]+', ' ', howIsTheDay).lower()
 
@@ -100,17 +109,17 @@ while 1:
     wordsCnt = len(words)
 
 
-# analysis
-
+    # analysis
     if mode == "1":
-        # basic
+        # basic version
+
+        # use AC Automation to find keywords in the sentence efficiently
         positiveAnalysis = positive_acAutomation.matchAll(howIsTheDay)
         negativeAnalysis = negative_acAutomation.matchAll(howIsTheDay)
 
+        # deal with "not bad" or "not good" scenario
         reversedPositive = 0
         reversedNegative = 0
-
-        # deal with "not bad" scenario
         for i in range(wordsCnt-1):
             if words[i] == "not":
                 tempPositive = positive_acAutomation.matchAll(words[i+1])
@@ -129,16 +138,20 @@ while 1:
         # if the # of positive words exceeds that of negative words, consider as positive response
         sense = positiveCnt - negativeCnt
     else:
+        # google online sentiment analysis version
         sense = get_sentiment(howIsTheDay)
+
+    # randomize the answer
     answerIndex = random.randint(1, 100) % 2
 
+    # print basic info
     print("(You entered " + str(lettersCnt) + " letters and " + str(wordsCnt) + " words.)\n")
 
-    mechPrint(answer[sign(sense) + 1][answerIndex] + "\n")
-    t2sEngine.say(answer[sign(sense) + 1][answerIndex])
-    t2sEngine.runAndWait()
+    # print ans
+    # the answer pool is set up in the order of negative, neutral and positive.
+    # the sign of sentiment plus 1 will be the index of answer pool
+    mechOutput(answer[sign(sense) + 1][answerIndex])
 
     mechPrint("\n-------INITIALIZING-------\n")
 
-
-print("Bye")
+print("Bye\n")
